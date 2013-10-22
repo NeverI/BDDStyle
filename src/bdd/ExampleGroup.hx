ad79862a5e85a3f12a79c3f5937d216e2f73e408
NeverI <neverii@freemail.hu>
2013-10-22 06:19:18 +0200

    TestCase alias ExampleGroup added

---
+++ src/bdd/ExampleGroup.hx
@@ -0,0 +1,59 @@
+package bdd;
+
+import bdd.expection.CompositeShould;
+
+class ExampleGroup
+{
+    public static var describeTracker:DescribeTracker;
+
+    private var should:CompositeShould;
+
+    public function new(should:CompositeShould)
+    {
+        this.should = should;
+    }
+
+    public function beforeEach():Void
+    {
+    }
+
+    public function afterEach():Void
+    {
+    }
+
+    public function extendBeforeEach(method:Void->Void):Void
+    {
+        ExampleGroup.describeTracker.addBeforeEach(method);
+    }
+
+    public function extendAfterEach(method:Void->Void):Void
+    {
+        ExampleGroup.describeTracker.addAfterEach(method);
+    }
+
+    public function describe(msg:String, ?method:Void->Void):Void
+    {
+        var describe:Describe = new Describe(msg, method);
+
+        describe.isPending = ExampleGroup.describeTracker.isPending;
+        ExampleGroup.describeTracker.addRunnable(describe);
+    }
+
+    public function xdescribe(msg:String, ?method:Void->Void):Void
+    {
+        var describe:Describe = new Describe(msg, method);
+
+        describe.isPending = true;
+        ExampleGroup.describeTracker.addRunnable(describe);
+    }
+
+    public function it(msg:String, ?method:Void->Void):Void
+    {
+        ExampleGroup.describeTracker.addRunnable(new It(msg, method));
+    }
+
+    public function xit(msg:String, ?method:Void->Void):Void
+    {
+        ExampleGroup.describeTracker.addRunnable(new It(msg));
+    }
+}
