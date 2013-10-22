eb3bc7685f6f4cbcdb3d1a5463cbbd2c95b1f1cb
NeverI <neverii@freemail.hu>
2013-10-22 06:34:30 +0200

    Reporter factory added

---
+++ src/bdd/reporter/helper/Factory.hx
@@ -0,0 +1,18 @@
+package bdd.reporter.helper;
+
+class Factory
+{
+    private var infoCollector:InfoCollector;
+    private var printer:Printer;
+
+    public function new()
+    {
+        this.printer = new Printer();
+        this.infoCollector = new InfoCollector();
+    }
+
+    public function create(cls:Class<Dynamic>):Void
+    {
+        return Type.createInstance(cls, [this.printer, this.infoCollector ]);
+    }
+}
