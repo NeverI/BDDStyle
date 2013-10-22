e3aac3f229e2f72d02bb201173918f5fad02af93
NeverI <neverii@freemail.hu>
2013-10-22 20:55:00 +0200

    helper.Printer: code cleanup

--- src/bdd/reporter/helper/Printer.hx
+++ src/bdd/reporter/helper/Printer.hx
@@ -3,33 +3,39 @@ package bdd.reporter.helper;
 class Printer
 {
     #if flash9
-        private var textfield: flash.text.TextField = null;
+        private var textfield: flash.text.TextField;
     #elseif flash
-        private var textfield: flash.TextField = null;
+        private var textfield: flash.TextField;
+    #elseif js
+        private var textfield: js.html.Element;
     #end
 
     public function new()
     {
+        #if flash9
+            textfield = new flash.text.TextField();
+            textfield.selectable = false;
+            textfield.width = flash.Lib.current.stage.stageWidth;
+            textfield.autoSize = flash.text.TextFieldAutoSize.LEFT;
+            flash.Lib.current.addChild(textfield);
+        #elseif flash
+            var root = flash.Lib.current;
+            root.createTextField("__tf",1048500,0,0,flash.Stage.width,flash.Stage.height+30);
+            textfield = root.__tf;
+            textfield.selectable = false;
+            textfield.wordWrap = true;
+        #elseif js
+            textfield = js.Browser.document.getElementById("haxe:trace");
+            if( textfield == null ) {
+                js.Lib.alert("haxe:trace element not found");
+            }
+        #end
     }
 
     public dynamic function print( v : Dynamic ) untyped {
         #if flash9
-            if( textfield == null ) {
-                textfield = new flash.text.TextField();
-                textfield.selectable = false;
-                textfield.width = flash.Lib.current.stage.stageWidth;
-                textfield.autoSize = flash.text.TextFieldAutoSize.LEFT;
-                flash.Lib.current.addChild(textfield);
-            }
             textfield.appendText(v);
         #elseif flash
-            var root = flash.Lib.current;
-            if( textfield == null ) {
-                root.createTextField("__tf",1048500,0,0,flash.Stage.width,flash.Stage.height+30);
-                textfield = root.__tf;
-                textfield.selectable = false;
-                textfield.wordWrap = true;
-            }
             var s = flash.Boot.__string_rec(v,"");
             textfield.text += s;
             while( textfield.textHeight > flash.Stage.height ) {
@@ -45,11 +51,7 @@ class Printer
             cpp.Lib.print(v);
         #elseif js
             var msg = StringTools.htmlEscape(js.Boot.__string_rec(v,"")).split("\n").join("<br/>");
-            var d = document.getElementById("haxe:trace");
-            if( d == null )
-                alert("haxe:trace element not found")
-            else
-                d.innerHTML += msg;
+            textfield.innerHTML += msg;
         #elseif cs
             var str:String = v;
             untyped __cs__("System.Console.Write(str)");
