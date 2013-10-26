c7fc27a32e946640d59725766308948a0895a42f
NeverI <neverii@freemail.hu>
2013-10-26 10:06:08 +0200

    Smarter Grunt file
    configurable haxe commands per project level with platform specified
    compile and run exec commands
    
    compile_project_platform
    run_project_platform
    
    exec:compile_test_js
    exec:compile_example_flash
    
    exec:run_test_neko
    exec:run_example_cpp
    
    also register project_platform tasks where platform has run options
    
    test_neko (compile and run)
    example_cpp (compile and run)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,6 @@
 module.exports = function(grunt) {
 
   var
-    project = 'haxe -main TestMain -lib mockatoo -cp test ',
-    example = 'haxe -main TestMain -lib mockatoo -cp example ',
     littleColoring = function(err, stdout, stderr)
     {
         if (err || stderr) {
@@ -15,59 +13,96 @@ module.exports = function(grunt) {
         if (lastLine && lastLine.indexOf('OK') !== 0) {
             grunt.log.fail('\nSome test failed');
         }
+    },
+    projects = {
+        test: {
+            main: 'TestMain',
+            cmd: 'haxe -main %main% -lib mockatoo -cp test'
+        },
+        example: {
+            main: 'TestMain',
+            cmd: 'haxe -main %main% -lib mockatoo -cp example',
+        }
+    },
+    platforms = {
+        js: {
+            compile: '-js build/%project%/js.js'
+        },
+        flash: {
+            compile: '-swf build/%project%/flash.swf -swf-header 800:800:24',
+            run: {
+                command: 'flashplayerdebugger build/%project%/flash.swf'
+            }
+        },
+        neko: {
+            compile: '-neko build/%project%/neko.n',
+            run: {
+                command: 'neko build/%project%/neko.n',
+                callback: littleColoring
+            },
+        },
+        cpp: {
+            compile: '-cpp build/%project%/cpp',
+            run: {
+                command: 'build/%project%/cpp/%main%',
+                callback: littleColoring
+            },
+        },
+    },
+    createExecCommands = function(projects, platforms)
+    {
+        var exec = {};
+
+        for (projectName in projects) {
+            var project = projects[projectName];
+            project.cmd = project.cmd.replace('%main%', project.main);
+
+            for (platformName in platforms) {
+                var
+                    platform = platforms[platformName],
+                    commandNameSuffix = projectName+'_'+platformName;
+
+                exec['compile_'+commandNameSuffix] = {
+                    command: project.cmd + ' ' + platform.compile.replace('%project%', projectName)
+                };
+
+                if (platform.run) {
+                    exec['run_'+commandNameSuffix] = {
+                        callback: platform.callback,
+                        command: platform.run.command.replace('%project%', projectName).replace('%main%', project.main)
+                    };
+                }
+            }
+        }
+
+        return exec;
     };
 
-  grunt.initConfig({
-    watch: {
-      source: {
-        files: ['bdd/**/*.hx', 'test/bdd/**/*.hx'],
-        tasks: ['exec:compileNeko', 'exec:runNeko']
-      },
-      example: {
-        files: ['example/**/*.hx', 'bdd/**/*.hx'],
-        tasks: ['exec:compileExampleNeko', 'exec:runExampleNeko']
-      }
-    },
+    grunt.initConfig({
+        watch: {
+            test: {
+                files: ['bdd/**/*.hx', 'test/bdd/**/*.hx'],
+                tasks: 'test_neko'
+            },
+            example: {
+                files: ['example/**/*.hx', 'bdd/**/*.hx'],
+                tasks: 'example_neko'
+            }
+        },
 
-    exec: {
-        compileExampleNeko: {
-            command: example+'-neko build/example_test.n'
-        },
-        compileExampleJS: {
-            command: example+'-js build/js_example.js'
-        },
-        compileExampleFlash: {
-            command: example+'-swf build/flash_example.swf -swf-header 1000:1000:30'
-        },
-        compileExampleCPP: {
-            command: example+'-cpp build/example'
-        },
-        runExampleNeko: {
-            command: 'neko build/example_test.n',
-            callback: littleColoring
-        },
-        compileNeko: {
-            command: project+'-neko build/neko_test.n'
-        },
-        compileJS: {
-            command: project+'-js build/js_test.js'
-        },
-        compileFlash: {
-            command: project+'-swf build/flash_test.swf -swf-header 1000:1000:30'
-        },
-        compileCPP: {
-            command: project+'-cpp build/cpp'
-        },
-        runNeko: {
-            command: 'neko build/neko_test.n',
-            callback: littleColoring
+        exec: createExecCommands(projects, platforms)
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-exec');
+
+    for (projectName in projects) {
+        for (platformName in platforms) {
+            if (platforms[platformName].run) {
+                var taskName = projectName+'_'+platformName;
+                grunt.registerTask(taskName, ['exec:compile_' + taskName, 'exec:run_' + taskName])
+            }
         }
     }
-  });
-
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-exec');
-
-  grunt.registerTask('compile', ['exec:compileJS', 'exec:compileFlash', 'exec:compileNeko']);
-  grunt.registerTask('compileExample', ['exec:compileExampleJS', 'exec:compileExampleFlash', 'exec:compileExampleNeko']);
 };
