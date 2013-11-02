5d1f95eedd50181dbdf88ad2ba8cf392e8057e0d
NeverI <neverii@freemail.hu>
2013-11-02 13:47:26 +0100

    cli: help command updated

--- src/cli/command/Help.hx
+++ src/cli/command/Help.hx
@@ -2,8 +2,17 @@ package cli.command;
 
 class Help extends Command
 {
+    override public function printHelp():Void
+    {
+        trace('Global parameters:');
+        trace('-f <filepath> when your project file is not in the cwd or its name is not test');
+        trace('-p <platforms> comma separeted list for valid haxe or openfl platforms. Can specified which platfroms would you like run a command. With openfl default is neko.');
+    }
+
     override public function run():Void
     {
-        trace('this is the help command');
+        this.printHelp();
+
+        new Create(this.args, this.project, this.tools).printHelp();
     }
 }
