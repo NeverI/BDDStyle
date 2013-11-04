414536d61a2dc0b62d782940e6ed84cc87cf1198
NeverI <neverii@freemail.hu>
2013-11-04 07:09:02 +0100

    bdd: Silent reporter added for cpp and neko target

---
+++ bdd/reporter/Silent.hx
@@ -0,0 +1,22 @@
+package bdd.reporter;
+
+class Silent extends bdd.reporter.helper.Abstract
+{
+    public function new(printer:bdd.reporter.helper.Printer, infoCollector:bdd.reporter.helper.InfoCollector)
+    {
+        super(printer, infoCollector);
+
+        this.addListener('runner.done', this.printFinal);
+    }
+
+    private function printFinal(event:Dynamic):Void
+    {
+        if (this.infoCollector.failedSpecs.length != 0) {
+            #if (cpp|neko)
+            Sys.exit(1);
+            #else
+            this.print('failed');
+            #end
+        }
+    }
+}
