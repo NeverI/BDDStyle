7991d7055c2523b3648916382121ccf81dae1e24
NeverI <neverii@freemail.hu>
2013-11-08 06:26:14 +0100

    cli: Gruntfile.js colorizer copy pasted from tpl

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,155 @@
 module.exports = function(grunt) {
 
   var
-    littleColoring = function(err, stdout, stderr)
-    {
-        if (err || stderr) {
-            grunt.log.fail(err || stderr);
-            return;
-        }
+    okColor = 'green',
+    wrongColor = 'red',
+    pendingColor = 'cyan',
+    defaultColor = 'grey',
 
-        stdout = stdout.split('\n');
-        var lastLine = stdout[stdout.length - 2] ? stdout[stdout.length - 2] : stdout[stdout.length - 1];
-        if (lastLine && lastLine.indexOf('OK') !== 0) {
-            grunt.log.fail('\nSome test failed');
+    DotColorizer = {
+        line: '',
+
+        is: function(line)
+        {
+            this.line = line;
+            return this.isValidChar(0) && this.isValidChar(1) && this.isValidChar(2);
+        },
+
+        isValidChar: function(index)
+        {
+            return this.line.charAt(index) == '.' || this.line.charAt(index) == 'X' || this.line.charAt(index) == 'P' || this.line.charAt(index) == '';
+        },
+
+        colorize: function()
+        {
+            var line = '';
+            for (var i=0,len=this.line.length; i < len; i++) {
+                var ch = this.line[i];
+                line += ch[this.getColor(ch)];
+            }
+
+            return line;
+        },
+
+        getColor: function(ch)
+        {
+            if (ch == '.') {
+                return okColor;
+            } else if (ch == 'X') {
+                return wrongColor;
+            } else if (ch == 'P') {
+                return pendingColor;
+            }
+
+            return defaultColor;
         }
     },
+    SummaryColorizer = {
+        line: '',
+        pattern: /(\d+ spec), (\d+ success), (\d+ pending), (\d+ failed), (\d+ expects)/,
+
+        is: function(line)
+        {
+            this.line = line;
+            return line.match(this.pattern);
+        },
+
+        colorize: function()
+        {
+            var parts = this.line.split(',');
+
+            for (var i in parts) {
+                var part = parts[i];
+                parts[i] = part[this.getColor(part)];
+            }
+
+            return parts.join(','.grey);
+        },
+
+        getColor: function(part)
+        {
+            if (part.indexOf('success') != -1) {
+                return okColor;
+            } else if (part.indexOf('failed') != -1) {
+                return wrongColor;
+            } else if (part.indexOf('pending') != -1) {
+                return pendingColor;
+            }
+
+            return defaultColor;
+        }
+    },
+    DescriptiveColorizer = {
+        line: '',
+        matches: [],
+        searchPattern: /(\s+)([X|P]: )/,
+        replacePattern: /([X|P]: )/,
+
+        is: function(line)
+        {
+            this.line = line;
+            this.matches = this.line.match(this.searchPattern);
+            return this.matches;
+        },
+
+        colorize: function()
+        {
+            this.line = this.line.replace(this.replacePattern, '');
+            if (this.matches[2].indexOf('X') != -1) {
+                return this.line[wrongColor];
+            }
+
+            return this.line[pendingColor];
+        },
+    },
+    Highlighter = {
+        line: '',
+        lineIndex: 0,
+        dotted: Object.create(DotColorizer),
+        summary: Object.create(SummaryColorizer),
+        descriptive: Object.create(DescriptiveColorizer),
+
+        colorize: function()
+        {
+            return function(err, stdout, stderr) {
+                stdout = stdout.split('\n');
+                this.lineIndex = 1;
+                for (var i in stdout) {
+                    this.line = stdout[i];
+                    this.process();
+                    this.lineIndex++;
+                }
+            }.bind(this);
+        },
+
+        process: function()
+        {
+            if (this.lineIndex == 1 && this.dotted.is(this.line)) {
+                this.line = this.dotted.colorize()+'\n';
+
+            } else if (this.summary.is(this.line)) {
+                this.line = this.summary.colorize();
+
+            } else if (this.descriptive.is(this.line)) {
+                this.line = this.descriptive.colorize();
+
+            } else if (this.line.match(/\d+\)/)) {
+                this.line = this.line[wrongColor];
+
+            } else if (this.line == 'OK') {
+                this.line = this.line[okColor];
+
+            } else if (this.line == 'FAILED') {
+                this.line = this.line[wrongColor];
+
+            } else {
+                this.line = this.line[defaultColor];
+            }
+
+            grunt.log.writeln(this.line);
+        },
+    },
+    highlight = Highlighter.colorize(),
     projects = {
         test: {
             main: 'TestMain',
@@ -42,14 +178,14 @@ module.exports = function(grunt) {
             compile: '-neko build/%project%/neko.n',
             run: {
                 command: 'neko build/%project%/neko.n',
-                callback: littleColoring
+                callback: highlight
             },
         },
         cpp: {
             compile: '-cpp build/%project%/cpp',
             run: {
                 command: 'build/%project%/cpp/%main%',
-                callback: littleColoring
+                callback: highlight
             },
         },
     },
@@ -72,7 +208,8 @@ module.exports = function(grunt) {
 
                 if (platform.run) {
                     exec['run_'+commandNameSuffix] = {
-                        callback: platform.callback,
+                        callback: platform.run.callback,
+                        stdout: false,
                         command: platform.run.command.replace('%project%', projectName).replace('%main%', project.main)
                     };
                 }
