4178423370578ec686a251fe8f07324cc1385244
Gabor <neverii@freemail.hu>
2013-11-17 16:22:54 +0100

    cli: sys.io.Process changed to sys.command

--- src/cli/helper/Process.hx
+++ src/cli/helper/Process.hx
@@ -9,10 +9,10 @@ class Process
     public function runWithDefault(runnable:String):Int
     {
         if (Sys.systemName() == 'Linux') {
-            return this.run('xdg-open', [runnable]);
+            return Sys.command('xdg-open', [runnable]);
 
         } else if (Sys.systemName() == 'Mac') {
-            return this.run('open', [runnable]);
+            return Sys.command('open', [runnable]);
 
         } else if (Sys.systemName() == 'Windows') {
             return Sys.command('start', [runnable]);
@@ -23,15 +23,6 @@ class Process
 
     public function run(command:String, ?arguments:Array<String>):Int
     {
-        var process = new sys.io.Process(command, arguments != null ? arguments : []);
-
-        var output = process.stderr.readAll();
-        if (output.length != 0) {
-            throw output;
-        }
-
-        Sys.print(process.stdout.readAll());
-
-        return process.exitCode();
+        return Sys.command(command, arguments != null ? arguments : []);
     }
 }
